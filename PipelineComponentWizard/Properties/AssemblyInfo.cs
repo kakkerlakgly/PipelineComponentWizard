e2486f54a6ffc5069612b90aa616acf7ef4972b6
Kristian Rastrup <kristian@rastrup.net>
2021-04-08 13:05:56 +0200

    Change BizTalk 2016 to 2019

--- PipelineComponentWizard/Properties/AssemblyInfo.cs
+++ PipelineComponentWizard/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BizTalk Server 2016 Pipeline Component Wizard")]
+[assembly: AssemblyTitle("BizTalk Server 2019 Pipeline Component Wizard")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
